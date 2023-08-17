9a4fdb4bdaa63ea40ddc8095d67eebb8573ec516
lanreoke024 <write.lanreoke@gmail.com>
2023-08-17 16:49:05 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print decimals
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= 9);
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
