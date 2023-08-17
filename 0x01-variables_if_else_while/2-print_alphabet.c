bf936f27acc65544189540f339bc562b6f94e8ec
lanreoke024 <write.lanreoke@gmail.com>
2023-08-17 14:51:26 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char alphabets = 'a';
+
+	while (alphabets <= 'z')
+	{
+	putchar(alphabets);
+	alphabets++;
+	}
+	putchar("\n");
+	return (0);
+}
